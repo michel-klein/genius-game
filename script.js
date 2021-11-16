21250851a4288a232ed530dc2451ce3ab7118a03
michel-klein <michel.kps@outlook.com>
2021-11-15 23:04:25 -0300

    Files and main structure of the Genius game created

---
+++ script.js
@@ -0,0 +1,14 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+//0 - verde
+//1 - vermelho
+//2 - amarelo
+//3 - azul
+
+const green = document.querySelector('.green');
+const red = document.querySelector('.red');
+const yellow = document.querySelector('.yellow');
+const blue = document.querySelector('.blue');
+
